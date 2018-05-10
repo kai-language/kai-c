5825600d07937b44d497eda204b2f0cbdd23d0e2
vdka <ethanjackwitz@gmail.com>
2018-05-10 10:04:46 +1000

    Remove DeadCode, ArrayCount in STR, and tests.log

--- src/main.cpp
+++ src/main.cpp
@@ -37,39 +37,6 @@ void Usage() {
     printUsage(1, "-emit-times            Emit times for each stage of compilation");
 }
 
-
-#if 0
-void Backtrace() {
-    #if SYSTEM_POSIX
-        void* callstack[25];
-        int i, frames = backtrace(callstack, ArrayCount(callstack));
-        char** strs = backtrace_symbols(callstack, frames);
-        for (i = 0; i < frames; ++i) {
-            fprintf(stderr, "%s\n", strs[i]);
-        }
-        free(strs);
-    #elif SYSTEM_WINDOWS
-        UNIMPLEMENTED();
-    #endif
-}
-
-void assertHandler(char const *file, i32 line, char const *msg, ...) {
-    Backtrace();
-
-    fprintf(stderr, "Assert failure");
-
-    if (msg) {
-        fprintf(stderr, ": ");
-        va_list va;
-        va_start(va, msg);
-        vfprintf(stderr, msg, va);
-        va_end(va);
-    }
-
-    fprintf(stderr, "\n");
-}
-#endif
-
 #ifndef TEST
 int main(int argc, char **argv) {
     if (argc < 2) {
