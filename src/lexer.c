b806f6b4df320601dfc2efc58f49de2ae1cda03e
Brett R. Toomey <brto@nodes.dk>
2018-05-18 16:44:47 +0900

    merge conflict

--- src/lexer.c
+++ src/lexer.c
@@ -100,70 +100,72 @@ bool shouldInsertSemiAfterKeyword(const char *keyword) {
 }
 
 #define TOKEN_KINDS \
-    TKind(Eof, "EOF"), \
-    TKind(Comment, "comment"), \
-    TKind(Ident, "identifier"), \
-    TKind(Directive, "directive"), \
-    TKind(Int, "int"), \
-    TKind(Float, "float"), \
-    TKind(String, "string"), \
-    TKind(Add, "+"), \
-    TKind(Sub, "-"), \
-    TKind(Mul, "*"), \
-    TKind(Div, "/"), \
-    TKind(Rem, "%"), \
-    TKind(And, "&"), \
-    TKind(Or, "|"), \
-    TKind(Xor, "^"), \
-    TKind(Shl, "<<"), \
-    TKind(Shr, ">>"), \
-    TKind(AddAssign, "+="), \
-    TKind(SubAssign, "-="), \
-    TKind(MulAssign, "*="), \
-    TKind(DivAssign, "/="), \
-    TKind(RemAssign, "%="), \
-    TKind(AndAssign, "&="), \
-    TKind(OrAssign, "|="), \
-    TKind(XorAssign, "^="), \
-    TKind(ShlAssign, "<<="), \
-    TKind(ShrAssign, ">>="), \
-    TKind(Land, "&&"), \
-    TKind(Lor, "||"), \
-    TKind(Lss, "<"), \
-    TKind(Gtr, ">"), \
-    TKind(Not, "!"), \
-    TKind(Eql, "=="), \
-    TKind(Neq, "!="), \
-    TKind(Leq, "<="), \
-    TKind(Geq, ">="), \
-    TKind(Assign, "="), \
-    TKind(Ellipsis, ".."), \
-    TKind(Dollar, "$"), \
-    TKind(Question, "?"), \
-    TKind(RetArrow, "->"), \
-    TKind(Lparen, "("), \
-    TKind(Lbrack, "["), \
-    TKind(Lbrace, "{"), \
-    TKind(Rparen, ")"), \
-    TKind(Rbrack, "]"), \
-    TKind(Rbrace, "}"), \
-    TKind(Comma, ","), \
-    TKind(Dot, "."), \
-    TKind(Colon, ":"), \
-    TKind(Terminator, ";"), \
-    TKind(Keyword, "")
+    FOR_EACH(Eof, "EOF") \
+    FOR_EACH(Comment, "comment") \
+    FOR_EACH(Ident, "identifier") \
+    FOR_EACH(Directive, "directive") \
+    FOR_EACH(Int, "int") \
+    FOR_EACH(Float, "float") \
+    FOR_EACH(String, "string") \
+    FOR_EACH(Add, "+") \
+    FOR_EACH(Sub, "-") \
+    FOR_EACH(Mul, "*") \
+    FOR_EACH(Div, "/") \
+    FOR_EACH(Rem, "%") \
+    FOR_EACH(And, "&") \
+    FOR_EACH(Or, "|") \
+    FOR_EACH(Xor, "^") \
+    FOR_EACH(Shl, "<<") \
+    FOR_EACH(Shr, ">>") \
+    FOR_EACH(AddAssign, "+=") \
+    FOR_EACH(SubAssign, "-=") \
+    FOR_EACH(MulAssign, "*=") \
+    FOR_EACH(DivAssign, "/=") \
+    FOR_EACH(RemAssign, "%=") \
+    FOR_EACH(AndAssign, "&=") \
+    FOR_EACH(OrAssign, "|=") \
+    FOR_EACH(XorAssign, "^=") \
+    FOR_EACH(ShlAssign, "<<=") \
+    FOR_EACH(ShrAssign, ">>=") \
+    FOR_EACH(Land, "&&") \
+    FOR_EACH(Lor, "||") \
+    FOR_EACH(Lss, "<") \
+    FOR_EACH(Gtr, ">") \
+    FOR_EACH(Not, "!") \
+    FOR_EACH(BNot, "~") \
+    FOR_EACH(Eql, "==") \
+    FOR_EACH(Neq, "!=") \
+    FOR_EACH(Leq, "<=") \
+    FOR_EACH(Geq, ">=") \
+    FOR_EACH(Assign, "=") \
+    FOR_EACH(Ellipsis, "..") \
+    FOR_EACH(Dollar, "$") \
+    FOR_EACH(Question, "?") \
+    FOR_EACH(RetArrow, "->") \
+    FOR_EACH(Lparen, "(") \
+    FOR_EACH(Lbrack, "[") \
+    FOR_EACH(Lbrace, "{") \
+    FOR_EACH(Rparen, ")") \
+    FOR_EACH(Rbrack, "]") \
+    FOR_EACH(Rbrace, "}") \
+    FOR_EACH(Comma, ",") \
+    FOR_EACH(Dot, ".") \
+    FOR_EACH(Colon, ":") \
+    FOR_EACH(Terminator, ";") \
+    FOR_EACH(Keyword, "")
 
 typedef enum TokenKind TokenKind;
 enum TokenKind {
-#define TKind(e, s) TK_##e
+#define FOR_EACH(e, s) TK_##e,
     TOKEN_KINDS
-#undef TKind
+#undef FOR_EACH
+    NUM_TOKEN_KINDS,
 };
 
 const char *TokenDescriptions[] = {
-#define TKind(e, s) "" #s ""
+#define FOR_EACH(e, s) "" #s ""
     TOKEN_KINDS
-#undef TKind
+#undef FOR_EACH
 };
 
 const char *DescribeTokenKind(TokenKind tk) {
