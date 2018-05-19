78b69139f4992c17881494f00fc6e930b23b3fe1
Brett R. Toomey <brto@nodes.dk>
2018-05-19 23:53:34 +0900

    Removed Gm switch

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 set TARGET=kai.exe
 
-set CFLAGS= -nologo -Oi -TP -Gm -MP -FC -GS- -EHsc- -GR-
+set CFLAGS= -nologo -Oi -TP -MP -FC -GS- -EHsc- -GR-
 set CFLAGS=%CFLAGS% -Od -MDd -Z7
 
 set LFLAGS= kernel32.lib -incremental:no -opt:ref -subsystem:console -debug
