036065a0753e1a6fc03eb048ff93f9c514ef559b
Brett R. Toomey <brto@nodes.dk>
2018-05-19 23:51:47 +0900

    Updating build flags

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
+@echo off
+
 set TARGET=kai.exe
 
-set CFLAGS= -nologo -Oi -TP -Gm -MP -FC -GC- -EHsc -GR-
+set CFLAGS= -nologo -Oi -TP -Gm -MP -FC -GS- -EHsc- -GR-
 set CFLAGS=%CFLAGS% -Od -MDd -Z7
 
 set LFLAGS= kernel32.lib -incremental:no -opt:ref -subsystem:console -debug
