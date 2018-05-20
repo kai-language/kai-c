fca82e12bfcf49ebd099fbc7bf7b54a65ac93608
Brett R. Toomey <brto@nodes.dk>
2018-05-20 22:47:08 +0900

    Remove casts

--- src/os.c
+++ src/os.c
@@ -3,7 +3,7 @@ char *AbsolutePath(const char *filename, char *resolved) {
 #ifdef SYSTEM_POSIX
     return realpath(filename, resolved);
 #else
-    return GetFullPathName((LPCTSTR)filename, MAX_PATH, (LPTSTR)resolved, NULL);
+    return GetFullPathName(filename, MAX_PATH, resolved, NULL);
 #endif
 }
 
