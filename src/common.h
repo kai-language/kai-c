b48a55c731b0cff4f5f21d4b1575d0a1b63860fb
vdka <ethanjackwitz@gmail.com>
2018-06-09 21:54:30 +1000

    Update the MAX_PATH Macro

--- src/common.h
+++ src/common.h
@@ -62,13 +62,7 @@ extern "C" {
 #endif
 
 #ifndef MAX_PATH
-    #if defined _MAX_PATH
-        #define MAX_PATH _MAX_PATH
-    #elif defined PATH_MAX
-        #define MAX_PATH PATH_MAX
-    #else
-        #error "No suitable MAX_PATH surrogate"
-    #endif
+    #define MAX_PATH 4096
 #endif
 
 #define MIN(x, y) ((x) <= (y) ? (x) : (y))
